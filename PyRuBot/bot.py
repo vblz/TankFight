cdfc7004024dadef23f6b5e55e3ee1c14026a303
Ruslan Gabbasov <ruslan.gabbasov@gmail.com>
2018-11-02 00:57:51 +0500

    Python random bot

--- PyRuBot/bot.py
+++ PyRuBot/bot.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-
-import sys
+import random
 import json
 
 while True:
     state = input()
     gameState = json.loads(state)
-    sys.stderr.write(state)
-    print('[{"Type":1,"Direction":3}]')
+    myTank = next(c for c in gameState["ContentsInfo"] if c["Type"] == 0 and c["UserId"] == "pyrubot")["Coordinates"]
+    enemyTank = next(c for c in gameState["ContentsInfo"] if c["Type"] == 0 and c["UserId"] != "pyrubot")["Coordinates"]
+    dX = myTank["X"] - enemyTank["X"]
+    dY = myTank["Y"] - enemyTank["Y"]
+    if random.randint(0, 100) < 70:
+        direction = ((0 if dX < 0 else 2) if (abs(dX) > abs(dY)) else (1 if dY < 0 else 3))
+    else:
+        direction = (random.randint(0, 3))
+
+    print([{'Type': 0 if random.randint(0, 100) < 70 else 1, 'Direction': direction}])
 
