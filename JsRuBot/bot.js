c721cb7b0ac6fd147530a4715346c10bd3759c3d
Ruslan Gabbasov <ruslan.gabbasov@gmail.com>
2018-11-02 00:11:34 +0500

    JS random bot

--- JsRuBot/bot.js
+++ JsRuBot/bot.js
@@ -2,18 +2,26 @@ const linq = require('linq'),
     readline = require('readline'),
     log = console.log,
     rl = readline.createInterface(process.stdin, process.stdout),
-    botName = 'jsrubot';
+    botName = 'vblz/tanks:jsrubot';
+
+const UserActionType = { Move: 0, Shoot: 1},
+    Direction = {Left: 0, Up: 1, Right: 2, Down: 3},
+    CellType = {Tank: 0, Barrier: 1, NotDestroyable: 2, Water: 3};
 
 rl.on('line', (state) => {
     let userAction = [];
-    try {
     let gameState = JSON.parse(state);
-    let myTank = linq.from(gameState).firstOrDefault((c) => c.Type === "Tank" && c.UserId === botName).Coordinates;
-    let enemyTank = linq.from(gameState).firstOrDefault((c) => c.Type === "Tank" && c.UserId !== botName).Coordinates;
-    }
-    catch (e) {
-        log(state);
-    }
-    userAction.push({Type: 1, Direction: 3});
+    let myTank = linq.from(gameState.ContentsInfo).firstOrDefault((c) => c.Type === CellType.Tank && c.UserId === botName).Coordinates;
+    let enemyTank = linq.from(gameState.ContentsInfo).firstOrDefault((c) => c.Type === CellType.Tank && c.UserId !== botName).Coordinates;
+    let dX = myTank.X - enemyTank.X;
+    let dY = myTank.Y - enemyTank.Y;
+
+    let direction = (Math.random() > 0.7) ? (Math.floor(Math.random() * 4)) : (Math.abs(dX) > Math.abs(dY)
+        ? (dX < 0 ? Direction.Right : Direction.Left)
+        : (dY < 0 ? Direction.Up : Direction.Down));
+
+    let action = (Math.random() > 0.5) ? (UserActionType.Move) : (UserActionType.Shoot);
+
+    userAction.push({Type: action, Direction: direction});
     log(userAction);
 });
