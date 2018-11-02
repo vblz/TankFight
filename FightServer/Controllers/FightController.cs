f0e1243513dac587a3c209fb236768a5f119b65b
Блажнов Василий <Blazhnov_VV@directum.ru>
2018-11-02 17:25:57 +0500

    Revert "исправил ошибку после рефакторинга"
    This reverts commit 2bc74189b1e57ec112a1245df6279d60c0a47451.

--- FightServer/Controllers/FightController.cs
+++ FightServer/Controllers/FightController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using FightServer.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,7 @@ namespace FightServer.Controllers
     [ProducesResponseType(200)]
     [ProducesResponseType(400)]
     [ProducesResponseType(500)]
-    public IActionResult StartNew([FromBody] string[] dockerImages)
+    public async Task<IActionResult> StartNew([FromBody] string[] dockerImages)
     {
       if (dockerImages == null || dockerImages.Length == 0)
       {
@@ -35,7 +36,7 @@ namespace FightServer.Controllers
         return this.BadRequest();
       }
 
-      var battleInfo = this.battleService.StartNew(dockerImagesSet);
+      var battleInfo = await this.battleService.StartNew(dockerImagesSet);
 
       return this.Ok(battleInfo);
     }
